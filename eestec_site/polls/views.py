f6c016c57fadb7cfc1325b973db428441d3d837b
Andrei Petre <p31andrei@gmail.com>
2013-04-27 01:16:42 +0300

    POST to / from index.

--- eestec_site/polls/views.py
+++ eestec_site/polls/views.py
@@ -4,14 +4,24 @@ from django.views.decorators.http import require_http_methods
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from polls.models import Entry
+from django.template import RequestContext, loader
+
 @require_http_methods(["GET"])
 def index(request):
     if not request.user.is_authenticated():
         return render_to_response('login.html')
+
+    t = loader.get_template('hello.html')
     objects = Entry.objects.all()
     types = Entry.objects.values('file_type').distinct()
     langs = Entry.objects.values('language').distinct()
-    return render_to_response('hello.html',{"entries":objects,"list_types":types,"list_lang":langs})
+    c = RequestContext(request, {"entries":objects,"list_types":types,"list_lang":langs})
+
+    if request.method == 'POST':
+        print 'ceva POST'
+        HttpResponse('ceva')
+    else:
+        return HttpResponse(t.render(c))
 
 @require_http_methods(["GET"])
 def secret(request):
