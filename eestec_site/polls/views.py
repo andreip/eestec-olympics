9877ebd9649ffe684db4d4f2c4beefb9400e990a
Diana Popa <pdiana.maria91@gmail.com>
2013-04-26 21:40:43 +0300

    super hello.html

--- eestec_site/polls/views.py
+++ eestec_site/polls/views.py
@@ -3,13 +3,14 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
-
+from polls.models import Entry
 @require_http_methods(["GET"])
 def index(request):
     if not request.user.is_authenticated():
         return render_to_response('login.html')
+    objects = Entry.objects.all()
 
-    return render_to_response('hello.html')
+    return render_to_response('hello.html',{"entries":objects})
 
 @require_http_methods(["GET"])
 def secret(request):
