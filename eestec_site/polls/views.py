8416ff01ad4a1fc4a2652e5d32d12da2b76e2bed
Matei Pavaluca <mateipavaluca@yahoo.com>
2013-04-27 01:55:21 +0300

    fixed some crap

--- eestec_site/polls/views.py
+++ eestec_site/polls/views.py
@@ -6,21 +6,20 @@ from django.shortcuts import render_to_response
 from polls.models import Entry
 from django.template import RequestContext, loader
 
-@require_http_methods(["GET"])
 def index(request):
     if not request.user.is_authenticated():
         return render_to_response('login.html')
 
-    t = loader.get_template('hello.html')
-    objects = Entry.objects.all()
-    types = Entry.objects.values('file_type').distinct()
-    langs = Entry.objects.values('language').distinct()
-    c = RequestContext(request, {"entries":objects,"list_types":types,"list_lang":langs})
-
     if request.method == 'POST':
         print 'ceva POST'
-        HttpResponse('ceva')
+        return HttpResponse('ceva')
     else:
+        t = loader.get_template('hello.html')
+        objects = Entry.objects.all()
+        types = Entry.objects.values('file_type').distinct()
+        langs = Entry.objects.values('language').distinct()
+        c = RequestContext(request, {"entries":objects,"list_types":types,"list_lang":langs})
+
         return HttpResponse(t.render(c))
 
 @require_http_methods(["GET"])
