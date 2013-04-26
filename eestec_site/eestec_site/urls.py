d6af15d285650dab8325402caf6b0ffad546216d
Andrei Petre <p31andrei@gmail.com>
2013-04-26 15:55:42 +0300

    Add simple auth index.

--- eestec_site/eestec_site/urls.py
+++ eestec_site/eestec_site/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from polls import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'eestec_site.views.home', name='home'),
@@ -14,4 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
 )
--- eestec_site/polls/views.py
+++ eestec_site/polls/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from django.core.urlresolvers import reverse
+
+@require_http_methods(["GET"])
+def index(request):
+    if not request.user.is_authenticated():
+        return HttpResponse('Please login!')
+
+    return HttpResponse("welcome, nigga!")
