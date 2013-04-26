a577fed9ee871c8805ab953a54a1724b7620cf8d
Andrei Petre <p31andrei@gmail.com>
2013-04-27 02:00:31 +0300

    Use render_to_response.

--- eestec_site/polls/views.py
+++ eestec_site/polls/views.py
@@ -4,23 +4,22 @@ from django.views.decorators.http import require_http_methods
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from polls.models import Entry
-from django.template import RequestContext, loader
+from django.template import RequestContext
 
 def index(request):
     if not request.user.is_authenticated():
         return render_to_response('login.html')
 
+    types = Entry.objects.values('file_type').distinct()
+    langs = Entry.objects.values('language').distinct()
+
     if request.method == 'POST':
-        print 'ceva POST'
         return HttpResponse('ceva')
     else:
-        t = loader.get_template('hello.html')
         objects = Entry.objects.all()
-        types = Entry.objects.values('file_type').distinct()
-        langs = Entry.objects.values('language').distinct()
-        c = RequestContext(request, {"entries":objects,"list_types":types,"list_lang":langs})
+        params = {"entries":objects,"list_types":types,"list_lang":langs}
+        return render_to_response("hello.html", params, RequestContext(request))
 
-        return HttpResponse(t.render(c))
 
 @require_http_methods(["GET"])
 def secret(request):
