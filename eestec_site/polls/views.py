634628cd2f12cdeee491801dfe29860bc7262cff
Matei Pavaluca <mateipavaluca@yahoo.com>
2013-04-27 03:32:15 +0300

    Nu stiu ce am facut dar cautarea merge... sort of

--- eestec_site/polls/views.py
+++ eestec_site/polls/views.py
@@ -14,11 +14,17 @@ def index(request):
     langs = Entry.objects.values('language').distinct()
 
     if request.method == 'POST':
-        resp = ""
+        args = {}
         for entry in request.POST.items():
-            print "caut " + entry[0] + " cu valoarea " + entry[1] + '\n'
+            args[entry[0]] = entry[1]
 
-        return HttpResponse(resp)
+        # Don't ask
+        del args['csrfmiddlewaretoken']
+
+        objects = Entry.objects.filter(**ceva)
+        params = {"entries":objects, "list_types":types, "list_lang":langs}
+
+        return render_to_response("hello.html", params, RequestContext(request))
     else:
         objects = Entry.objects.all()
         params = {"entries":objects,"list_types":types,"list_lang":langs}
